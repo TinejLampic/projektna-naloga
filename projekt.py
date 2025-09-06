9d74df8baddf3da8fc2f3471465f26ac72d8f736
Tinej Lampič <tl90991@student.uni-lj.si>
2025-09-06 15:28:43 +0200

    urejanje komentarjev

--- projekt.py
+++ projekt.py
@@ -6,37 +6,30 @@ import re
 
 #pridobitev podatkov iz spleta
 
-# definirajte URL glavne strani bolhe za oglase z mačkami
-realestate_frontpage_url = "https://www.bolha.com/hitro-iskanje?categoryIds%5B%5D=9579%2C9580%2C10920&geo%5Blat%5D=46.044463358034&geo%5Blng%5D=14.485205411911&geo%5BautoComplete%5D=Petrol+-+Ljubljana+-+Tr%C5%BEa%C5%A1ka+44%2C+Tr%C5%BEa%C5%A1ka+cesta+44%2C+1000+Ljubljana%2C+Slovenija&geo%5Bradius%5D=100"   #bolje da imena konstant pišemo z velikimi črkami
-# mapa, v katero bomo shranili podatke
-realestate_directory = 'podatki'
-# ime datoteke v katero bomo shranili glavno stran
+nepremicnine_frontpage_url = "https://www.bolha.com/hitro-iskanje?categoryIds%5B%5D=" \
+"9579%2C9580%2C10920&geo%5Blat%5D=46.044463358034&geo%5Blng%5D=14.485205411911&geo%5BautoComplete%5D=Petrol+-" \
+"+Ljubljana+-+Tr%C5%BEa%C5%A1ka+44%2C+Tr%C5%BEa%C5%A1ka+cesta+44%2C+1000+Ljubljana%2C+Slovenija&geo%5Bradius%5D=100" 
+
+nepremicnine_directory = 'podatki'  
+
 frontpage_filename = 'nepremicnine.html'
-# ime CSV datoteke v katero bomo shranili podatke
+
 csv_filename = 'nepremicnine.csv'
 
 
-def download_url_to_string(url):
-    """Funkcija kot argument sprejme niz in poskusi vrniti vsebino te spletne
-    strani kot niz. V primeru, da med izvajanje pride do napake vrne None.
-    """
+def download_url_to_string(url):   #funkcija v obliki niza vrne vsebino spletne strani
     try:
         # del kode, ki morda sproži napako
-        headers = {"User-Agent": "Chrome/139.0.7258.139"}     #da spletna stran ne misli da smo bot:  Chrome/verzija chroma
-        page_content = requests.get(url, headers=headers,).text   #v terminal napišemo: python -m pip install requests
+        headers = {"User-Agent": "Chrome/139.0.7258.139"}     #da spletna stran ne 'misli' da smo bot
+        page_content = requests.get(url, headers=headers,).text   
     except requests.exceptions.RequestException:
         # koda, ki se izvede pri napaki
-        # dovolj je če izpišemo opozorilo in prekinemo izvajanje funkcije
         print("Spletna stran ni dosegljiva.")
-    # nadaljujemo s kodo če ni prišlo do napake
     return page_content
 
 
-def save_string_to_file(text, directory, filename):
-    """Funkcija zapiše vrednost parametra "text" v novo ustvarjeno datoteko
-    locirano v "directory"/"filename", ali povozi obstoječo. V primeru, da je
-    niz "directory" prazen datoteko ustvari v trenutni mapi.
-    """
+def save_string_to_file(text, directory, filename):   #vrednost paprametra 'text' zapiše v novo ustvarjeno datoteko
+
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
     with open(path, 'w', encoding='utf-8') as file_out:
@@ -44,11 +37,7 @@ def save_string_to_file(text, directory, filename):
     return None
 
 
-# Definirajte funkcijo, ki prenese glavno stran in jo shrani v datoteko.
-
-
-def save_multiple_pages(base_url, directory, filename_prefix, num_pages=200):
-    """Shrani več zaporednih strani (npr. 10 strani oglasov) v mape."""
+def save_multiple_pages(base_url, directory, filename_prefix, num_pages=200):   #funkcija, ki shrani strani, vsako v svojo html datoteko
     all_content = ""
     for page_num in range(1, num_pages + 1):
         url = f"{base_url}?page={page_num}"
@@ -59,11 +48,7 @@ def save_multiple_pages(base_url, directory, filename_prefix, num_pages=200):
     return all_content
 
 
-
-###############################################################################
 # Po pridobitvi podatkov jih želimo obdelati.
-###############################################################################
-
 
 def read_file_to_string(directory, filename):   #odpremo datoteko za branje
     """Funkcija vrne celotno vsebino datoteke "directory"/"filename" kot niz."""
@@ -73,39 +58,21 @@ def read_file_to_string(directory, filename):   #odpremo datoteko za branje
         text = file_in.read()
     return text
     
-
-
-# Definirajte funkcijo, ki sprejme niz, ki predstavlja vsebino spletne strani,
-# in ga razdeli na dele, kjer vsak del predstavlja en oglas. To storite s
-# pomočjo regularnih izrazov, ki označujejo začetek in konec posameznega
-# oglasa. Funkcija naj vrne seznam nizov.
-
-
-def page_to_ads(page_content):
-    """Funkcija poišče posamezne oglase, ki se nahajajo v spletni strani in
-    vrne seznam oglasov."""
+def page_to_ads(page_content):   #funkcija sprejme niz in poišče posamezne oglase, ki jih nato vrne v obliki seznama
     return re.findall(r'<article class="entity-body cf">(.*?)</article>', page_content, flags=re.DOTALL)    
-    # *? - da se ustavi res samo pri koncu prvega oglasa / flags... da pika(.) res pomeni vse (drugace ne zavzame naslednje vrstice)
+    
     
 
-
-# Definirajte funkcijo, ki sprejme niz, ki predstavlja oglas, in izlušči
-# podatke o imenu, lokaciji, datumu objave in ceni v oglasu.
-
-def get_dict_from_ad_block(block):
-    """Funkcija iz niza za posamezen oglasni blok izlušči podatke o imenu, ceni
-    in opisu ter vrne slovar, ki vsebuje ustrezne podatke."""
+def get_dict_from_ad_block(block):    #funkcija za posamezen oglas izlušči željene podatke in jih vrne v slovarju
     lokacija = re.search(r'Lokacija: </span>(.*)<br />', block)
     povrsina = re.search(r'Bivalna površina:\s*([\d,\.]+)\s*m2', block)
     datum = re.search(r'pubdate="pubdate">(.*)\.</time>', block)
     razdalja = re.search(r'Razdalja:\s*([\d,\.]+\s*(?:m|km))', block)
     cena = re.search(r'<strong class="price price--hrk">(.*)</strong>', block, flags=re.DOTALL)
 
-    if lokacija == None or povrsina == None or datum == None or razdalja == None or cena == None:
+    if lokacija == None or povrsina == None or datum == None or razdalja == None or cena == None:   #poskrbi, da ne upoštevamo oglasa, pri katerem kakšen podatek majnka
         print("Napaka v bloku:", block)
         return None
-
-
 
     return {
         'lokacija': lokacija.group(1),
@@ -116,12 +83,8 @@ def get_dict_from_ad_block(block):
     }
 
 
-# Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
-# besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
-# vseh oglasih strani.
 
-
-def ads_from_files(directory, filename_prefix, num_pages):
+def ads_from_files(directory, filename_prefix, num_pages):   #funkcija prebere posamezne strani oglasov (Html datoteke), iz njih izlušči bloke z oglasi, jih pretvori v slovarje s podatki in vse skupaj združi v seznam oglasov
     ads = []
     for page_num in range(1, num_pages + 1):
         filename = f"{filename_prefix}_{page_num}.html"
@@ -131,63 +94,39 @@ def ads_from_files(directory, filename_prefix, num_pages):
         ads.extend([ad for ad in page_ads if ad is not None])
     return ads
 
-###############################################################################
-# Obdelane podatke želimo sedaj shraniti.
-###############################################################################
 
+# Obdelane podatke želimo sedaj shraniti.
 
 def write_csv(fieldnames, rows, directory, filename):
-    """
-    Funkcija v csv datoteko podano s parametroma "directory"/"filename" zapiše
-    vrednosti v parametru "rows" pripadajoče ključem podanim v "fieldnames"
-    """
-    os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, filename)
+    os.makedirs(directory, exist_ok=True)   #ustvari mapo, če ne obstaja
+    path = os.path.join(directory, filename)       #sestavi pot
     with open(path, 'w', encoding='utf-8', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
+        writer.writeheader()       #zapiše imena stolpcev
         for row in rows:
             writer.writerow(row)   #v vrstici bodo podatki o eni nepremičnini
     return
 
 
-# Definirajte funkcijo, ki sprejme neprazen seznam slovarjev, ki predstavljajo
-# podatke iz oglasa mačke, in zapiše vse podatke v csv datoteko. Imena za
-# stolpce [fieldnames] pridobite iz slovarjev.
 
 
-def write_realestate_ads_to_csv(ads, directory, filename):
-    """Funkcija vse podatke iz parametra "ads" zapiše v csv datoteko podano s
-    parametroma "directory"/"filename". Funkcija predpostavi, da so ključi vseh
-    slovarjev parametra ads enaki in je seznam ads neprazen."""
-    # Stavek assert preveri da zahteva velja
-    # Če drži se program normalno izvaja, drugače pa sproži napako
-    # Prednost je v tem, da ga lahko pod določenimi pogoji izklopimo v
-    # produkcijskem okolju
+def write_nepremicnine_ads_to_csv(ads, directory, filename):   #funkcija, ki zapiše neprazen seznam slovarjev v csv datoteko
     assert ads and (all(j.keys() == ads[0].keys() for j in ads))    #assert sproži napako, če pogoj ni izpolnjen (če je seznam oglasov definiran in če imajo vsi slovarji v seznamu enake ključe)
-    fieldnames = list(ads[0].keys())                                #ključe spremenimo v seznam
+    fieldnames = list(ads[0].keys())                                #ključe spremenimo v seznam, to so imena stolpceb
     write_csv(fieldnames, ads, directory, filename)
 
 
 # Celoten program poženemo v glavni funkciji
 
-def main(redownload=True, reparse=True):
-    """Funkcija izvede celoten del pridobivanja podatkov:
-    1. Oglase prenese iz bolhe
-    2. Lokalno html datoteko pretvori v lepšo predstavitev podatkov
-    3. Podatke shrani v csv datoteko
-    """
-    # Najprej v lokalno datoteko shranimo glavno stran
-    if redownload:
-        save_multiple_pages(realestate_frontpage_url, realestate_directory, "nepremicnine", num_pages=200)
+def main(redownload=True, reparse=True):    #ta funkcija izvede celoten del pridobivanja podatkov
 
-    # Iz lokalne (html) datoteke preberemo podatke
-    # Podatke preberemo v lepšo obliko (seznam slovarjev)
-    # Podatke shranimo v csv datoteko
-    if reparse:
-        num_pages = 200  # prilagodi, koliko strani želiš
-        ads = ads_from_files(realestate_directory, "nepremicnine", num_pages)
-        write_realestate_ads_to_csv(ads, realestate_directory, csv_filename)
+    if redownload:   # Najprej v lokalno datoteko prenesemo strni
+        save_multiple_pages(nepremicnine_frontpage_url, nepremicnine_directory, "nepremicnine", num_pages=200)
+
+    if reparse:    #iz datotek prebere podatke, jih uredi in shrani v csv
+        num_pages = 200 
+        ads = ads_from_files(nepremicnine_directory, "nepremicnine", num_pages)
+        write_nepremicnine_ads_to_csv(ads, nepremicnine_directory, csv_filename)
 
 if __name__ == '__main__':    #če datoteko vključimo drugam, se nam main() ne izvede
     main(True)
